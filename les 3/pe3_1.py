fa3027db06f7be15452fee64774a998010a23aeb
nicoknottnerus <nico.knottnerus@student.hu.nl>
2017-09-18 11:43:35 +0200

    programeren les 2

--- les 3/pe3_1.py
+++ les 3/pe3_1.py
@@ -1,5 +1,5 @@
 score = eval(input('hoeveel punten heb je..'))
 if score > 14:
-    print('gefeliciteerd je bent geslaagt!')
+    print('gefeliciteerd je bent geslaagd!')
 else:
-    print('Helaas heb je keihard gefaalt!')
+    print('Helaas heb je keihard gefaald!')
